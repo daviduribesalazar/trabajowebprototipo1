9e2e140f9be42ab10a8644bc55fe2187dcd1f481
daviduribesalazar <93312286+daviduribesalazar@users.noreply.github.com>
2022-01-18 17:11:51 +0100

    Add files via upload

---
+++ src/views/receta1.vue
@@ -0,0 +1,53 @@
+<template>
+<div id="receta1">
+     <h1>Pollo teryiaki</h1>
+
+<div>
+  <b-img src="../assets/polloteryiaki.jpg" fluid alt="Responsive image"></b-img>
+</div>
+
+     <div class="ingredientes1">
+      <p><strong>Ingredientes</strong>.</p>
+      <ul>
+      <li>2 pechugas de pollo sin filetear</li>
+      <li>Semillas de sésamo al gusto
+      </li>
+      <li>Salsa teriyaki casera (hecha con 100 ml de salsa de soja, 50 ml de sake, 50 gr de azúcar y 50 ml de mirin)
+    </li>
+      <li>Arroz basmati para guarnición</li>
+      <li>Aceite de oliva virgen extra un par de cucharadas
+    </li>
+      <li>Sal y pimienta al gusto
+    </li>
+      <li>1 cebolleta (la parte verde)
+    </li>
+      <li>Jengibre fresco un trocito de medio cm (opcional)
+    </li>
+      </ul>
+    </div>
+    <div class="preparacion1">
+        <p><strong>Preparación</strong>.</p>
+        <p>Para hacer la salsa teriyaki casera, ponemos en un cazo la salsa de soja y añadimos el mirin, el azúcar, el sake y un trocito de jengibre. Calentamos a fuego fuerte hasta que hierva, mientras removemos hasta que el azúcar esté disuelto. Después dejamos que la salsa cueza y reduzca durante cinco minutos, siempre sin parar de remover. Apagamos, retiramos el jengibre y dejamos reposar mientras hacemos el pollo.
+        <br>
+        Limpiamos las pechugas de pollo retirando las telillas y ternillas que pueda tener y las cortamos en dados de un par de centímetros. Cortamos la parte verde de la cebolleta en juliana y la salteamos con dos cucharadas de aceite de oliva. Agregamos el pollo.
+        <br>
+        Dejamos que se dore por todas sus caras, lo que nos llevará unos cinco minutos y agregamos la salsa teriyaki casera. Dejamos que el conjunto se cocine durante quince minutos. Cuando veamos que el pollo está brillante y la salsa prácticamente se haya consumido, apagamos el fuego.
+        <br>
+        Servimos el pollo teriyaki en un cuenco, espolvoreamos con semillas de sésamo y acompañamos de una guarnición de arroz blanco, preferiblemente alargado, tipo jazmín o basmati.
+</p>
+      </div>
+
+
+
+
+</div>
+
+</template>
+
+<script>
+export default {
+   name: 'receta1',
+   components: {
+  }
+}
+</script>
